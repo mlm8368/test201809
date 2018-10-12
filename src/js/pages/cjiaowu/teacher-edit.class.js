d0006f66a36846fd89ef2ed0b9822f1a22a7025f
Mobile User <mobile@peterde-ipad.local>
2018-10-12 23:54:30 +0800

    ssssssssss

--- src/js/pages/cjiaowu/teacher-edit.class.js
+++ src/js/pages/cjiaowu/teacher-edit.class.js
@@ -5,4 +5,29 @@ export default class TeacherEdit extends Home {
     super()
     return this
   }
+  
+  editTeacher() {
+  
+    this.Vue.$notice.loading.show('正在提交...')
+    this.Vue.$fetch({
+      method: 'POST',
+      name: 'modules.classes',
+      params: { query: '&action=teacher&op=edit' },
+      header: this.ajaxHeader(),
+      data: this.Vue.formData
+    }).then(ret => {
+      this.Vue.$notice.loading.hide()
+
+      if (ret.status === 1) {
+        this.Vue.$parent.getTeacherLists('refresh')
+
+        this.Vue.$notice.alert({ message: '修改成功', callback: () => {
+          this.Vue.$parent.closeDialog()
+        } })
+      } else {
+        this.Vue.$notice.alert({ message: ret.msg })
+      }
+    }, error => {
+    })
+  }
 }
--- src/js/pages/cjiaowu/teacher-edit.vue
+++ src/js/pages/cjiaowu/teacher-edit.vue
@@ -1,8 +1,9 @@
 <template>
   <div>
     <bui-list-item label="教师职务" >
-      <input :value="formData.teacherpost" @input="" slot="title" class="input" placeholder="请输入公司名称" />
+      <input :value="formData.teacherpost" @input="onTeacherpost" slot="title" class="input" placeholder="请输入公司名称" />
     </bui-list-item>
+    <div class="teacherInfo-button"><am-button width="500px" size="small" text="添加" @click="buttonClick"></am-button></div>
   </div>
 </template>
 
@@ -13,13 +14,14 @@
 
 <script>
 import BuiListItem from '../../components/bui-list-item.vue'
+import AmButton from '../../components/am-button.vue'
 import TeacherEdit from './teacher-edit.class'
 
 const teacher = new TeacherEdit()
 
 export default {
   name: 'teacher-edit',
-  components: { BuiListItem },
+  components: { BuiListItem，AmButton },
   props: {
     index: {
       type: Number,
@@ -28,7 +30,7 @@ export default {
   },
   data () {
     return {
-      formData: { teacherpost: '' }
+      formData: { id: 0, teacherpost: '' }
     }
   },
   computed: {
@@ -36,10 +38,19 @@ export default {
   },
   created () {
     teacher.setVue(this)
+  
+    const teacherInfo = this.teacherLists[this.index]
+    if (teacherInfo > 0) {
+       this.formData.id = teacherInfo.id
+       this.formData.teacherpost = teacherInfo.teacherpost
+    }
   },
   methods: {
     onTeacherpost (e) {
       this.formData.teacherpost = e.value
+    },
+    buttonClick () {
+      teacher.editTeacher()
     }
   }
 }
