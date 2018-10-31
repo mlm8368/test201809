1c55bd599e38a64f5f4045e977a0e7c1c803f1eb
mlm8368 <ym001@126.com>
2018-10-31 10:42:01 +0800

    s

---
+++ src/js/pages/cxiaowu/vip.class.js
@@ -0,0 +1,8 @@
+import App from './home.class'
+
+export default class Vip extends App {
+  constructor() {
+    super()
+    return this
+  }
+}
---
+++ src/js/pages/cxiaowu/vip.vue
@@ -0,0 +1,22 @@
+<template>
+  <div><text>vip{{tt}}</text></div>
+</template>
+
+<style lang="less" scoped>
+
+</style>
+
+<script>
+import Vip from './vip.class'
+
+const vip = new Vip()
+
+export default {
+  name: 'vip',
+  data () {
+    return {
+      tt: 'ddd'
+    }
+  }
+}
+</script>
