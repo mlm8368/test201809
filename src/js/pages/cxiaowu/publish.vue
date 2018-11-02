ba05a3b7834cbc6895cae74d4d86718f436ce0fe
mlm8368 <ym001@126.com>
2018-11-02 11:19:54 +0800

    s

--- src/js/pages/cxiaowu/publish.vue
+++ src/js/pages/cxiaowu/publish.vue
@@ -1,21 +1,35 @@
 <template>
-  <div><text>publish{{tt}}</text></div>
+  <div class="publish">
+    
+  </div>
 </template>
 
 <style lang="less" scoped>
+@import "../../../css/variable.less";
+@import "../../../css/common.less";
 
 </style>
 
 <script>
+import BuiListItem from '../../components/bui-list-item.vue'
 import Publish from './publish.class'
 
 const publish = new Publish()
 
 export default {
   name: 'publish',
+  components: { BuiListItem },
   data () {
     return {
       tt: 'ddd'
+    }
+  },
+  created() {
+    publish.setVue(this)
+  },
+  methods: {
+    buttonClick () {
+      publish.log(this.formData)
     }
   }
 }
